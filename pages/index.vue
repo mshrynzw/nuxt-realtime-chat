b25f36e04d75b512c0048b054db985196278bbc3
mshrynzw <iq87io25@gmail.com>
2024-09-04 09:25:08 +0900

    Add: Design

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,71 @@
 <template>
-  <div>
-    <div v-if="user">
-      <p>Total messages: {{ messages.length }}</p>
-      <div v-for="message in messages" :key="message.id">
-        <p>{{ message.content }} ({{ message.user_id }})</p>
-      </div>
-      <input
-        v-model="newMessage"
-        placeholder="Type a message"
-        @keyup.enter="sendMessage"
-      />
-      <button @click="sendMessage">Send</button>
-      <button @click="logout">ログアウト</button>
-    </div>
-    <div v-else>
-      <p>
-        Please
-        <nuxt-link to="/login">log in</nuxt-link>
-        to join the chat.
-      </p>
+  <div class="relative min-h-screen flex flex-col">
+    <div
+      class="bg-gray-100 flex-grow flex items-center py-16 dark:bg-neutral-800"
+    >
+      <main class="w-full mx-auto p-6">
+        <div
+          class="mt-7 sbg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700"
+        >
+          <div class="p-4 sm:p-7">
+            <div class="text-center">
+              <h1
+                class="mb-7 text-4xl sm:text-6xl font-bold text-gray-800 dark:text-neutral-200"
+              >
+                Realtime Chat
+              </h1>
+
+              <div v-if="user">
+                <div
+                  class="relative z-10 flex gap-x-3 p-3 bg-white border rounded-lg shadow-lg shadow-gray-100 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-gray-900/20"
+                >
+                  <div class="w-full flex space-x-2">
+                    <input
+                      v-model="newMessage"
+                      placeholder="Type a message"
+                      class="py-2.5 px-4 block w-full border-transparent rounded-lg focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                      @keyup.enter="sendMessage"
+                    />
+                    <button
+                      class="m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                      @click="sendMessage"
+                    >
+                      Send
+                    </button>
+                    <button
+                      class="m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                      @click="logout"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+                <p class="mt-3 text-gray-600 dark:text-neutral-400">
+                  Total messages: {{ messages.length }}
+                </p>
+                <div
+                  v-for="message in messages"
+                  :key="message.id"
+                  class="text-left mt-3 text-gray-800 dark:text-neutral-200"
+                >
+                  <p>
+                    <span>{{ formatDate(message.created_at) }}</span>
+                    :
+                    <span class="font-bold">{{ message.content }}</span>
+                  </p>
+                </div>
+              </div>
+              <div v-else>
+                <p>
+                  Please
+                  <nuxt-link to="/login">log in</nuxt-link>
+                  to join the chat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
@@ -32,10 +79,10 @@ import {
   useRoute,
 } from '@nuxtjs/composition-api'
 import {
-  RealtimeChannel,
   RealtimePostgresChangesPayload,
 } from '@supabase/supabase-js'
 import { onMounted, onUnmounted } from 'vue'
+
 
 export default defineComponent({
   setup() {
@@ -47,6 +94,20 @@ export default defineComponent({
     const messages = ref<any[]>([])
     const newMessage = ref('')
     let subscription: any = null
+
+    const formatDate = (dateString: string) => {
+      const date = new Date(dateString)
+      const options: Intl.DateTimeFormatOptions = {
+        timeZone: 'Asia/Tokyo',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      }
+      return new Intl.DateTimeFormat('ja-JP', options).format(date)
+    }
 
     const fetchUser = async () => {
       const {
@@ -63,7 +124,7 @@ export default defineComponent({
       const { data, error } = await $supabase
         .from('messages')
         .select('*')
-        .order('created_at', { ascending: true })
+        .order('created_at', { ascending: false })
 
       if (error) console.error('Error fetching messages:', error.message)
       else {
@@ -143,7 +204,7 @@ export default defineComponent({
       }
     })
 
-    return { user, messages, newMessage, sendMessage, logout }
+    return { user, messages, newMessage, sendMessage, logout, formatDate }
   },
 })
 </script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>
-    <input v-model="email" placeholder="Email" />
-    <input v-model="password" type="password" placeholder="Password" />
-    <button @click="signIn">Login</button>
-    <button @click="signUp">Sign Up</button>
+  <div class="relative min-h-screen flex flex-col">
+    <div
+      class="bg-gray-100 flex-grow flex items-center py-16 dark:bg-neutral-800"
+    >
+      <main class="w-full max-w-md mx-auto p-6">
+        <div
+          class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700"
+        >
+          <div class="p-4 sm:p-7">
+            <div class="text-center">
+              <h1
+                class="block text-2xl font-bold text-gray-800 dark:text-white"
+              >
+                Realtime Chat
+              </h1>
+            </div>
+
+            <div class="mt-5">
+              <div class="grid gap-y-4">
+                <div>
+                  <label for="email" class="block text-sm mb-2 dark:text-white"
+                    >Email</label
+                  >
+                  <div class="relative">
+                    <input
+                      v-model="email"
+                      placeholder="Email"
+                      name="email"
+                      class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div class="flex justify-between items-center">
+                    <label
+                      for="password"
+                      class="block text-sm mb-2 dark:text-white"
+                      >Password</label
+                    >
+                  </div>
+                  <div class="relative">
+                    <input
+                      v-model="password"
+                      type="password"
+                      placeholder="Password"
+                      class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <button
+                  class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  @click="signIn"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
